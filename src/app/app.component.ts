e7c7175ef838573c48aab02053f32db127506457
Marcos Augusto <msaorc@hotmail.com>
2023-01-06 18:32:45 -0300

    Project creation

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'expense-root',
+  templateUrl: 'app.component.html',
+})
+export class AppComponent {
+  title = 'ExpenseControlApp';
+}
